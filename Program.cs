fcedef431f5b9a7e6426f74582aa58b7e9839c9a
Martin Pasche <martin.pasche@uc.cl>
2020-07-23 17:14:47 -0400

    c#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gradebook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "alo";
+            string name2 = "alo";
+            if (name == name2) {
+                Console.WriteLine(true);
+            } else {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
